505b243c7a09eeb06b484b00d2dbc8b55d518e9f
Jay Shaffstall <jshaffstall@gmail.com>
2018-09-24 14:47:23 -0400

    Update filters.py
    First pass at categories filter

--- _python/filters.py
+++ _python/filters.py
@@ -1,5 +1,16 @@
+from .hooks import data
+
 def dateformat(value, format="%d-%b-%Y"):
     return value.strftime(format)
 
+# The value passed in should be a category name
+def categories(value):
+    for category in data:
+        if category == value:
+            return data[category]
+        
+    return []
+
 filters = {}
 filters['dateformat'] = dateformat
+filters['categories'] = categories
