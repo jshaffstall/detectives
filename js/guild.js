ebae257b22f868cd400baae71979124f7b858c4e
jshaffstall <jshaffstall@gmail.com>
2018-10-28 12:06:19 -0400

    Javascript tries

--- js/guild.js
+++ js/guild.js
@@ -1,4 +1,12 @@
 function doLogout ()
 {
-    $.get('https://detectives-guild-site.anvil.app/_/api/user/logout');
+    $.ajax({
+       url: 'https://detectives-guild-site.anvil.app/_/api/user/logout',
+       type: 'GET',
+       contentType: 'text/plain',
+       xhrFields: {
+          withCredentials: true
+       },
+       crossDomain: true
+    });    
 }
