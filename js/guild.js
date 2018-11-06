3bd0cd6bc0524b76db5b3dbc05b4abbb17358f52
Jay <jshaffstall@gmail.com>
2018-11-06 10:47:54 -0500

    More on processing messages from iframes

--- js/guild.js
+++ js/guild.js
@@ -2,12 +2,40 @@ $(document).ready(function()
 {
     window.addEventListener("message", receiveMessage);
 
-    //$(function () {
     function receiveMessage(event)
     {
         if (event.data && event.data.hasOwnProperty('type') && event.data.type == 'refresh')
         {
-            alert(event.data.sender);
+            // We need to refresh every Detectives Guild iframe
+            // except for the one that sent the message
+            //
+            // Every Detectives Guild iframe must be defined with 
+            // a data-guild attribute
+            
+            var iframes = document.querySelectorAll("iframe[data-guild]");
+            
+            for (var i in iframes) 
+            {
+                i = $(i)
+                
+                if (i.contentWindow != event.source)
+                    refreshIframe(i);
+            }            
         }
     }
 });
+
+function refreshIframe(iframe, src="")
+{
+    // This hackish technique prevents the history from being modified 
+    // when we reload the iframe
+    var container = iframe.parent();
+    
+    if (src == "")
+        src = iframe.src
+    
+    iframe.remove ();
+    iframe.attr('src', src);
+    
+    container.append(iframe);
+}
